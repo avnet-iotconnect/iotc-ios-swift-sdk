d573af63fa835df043deeca73fa4c465d5841f04
sw-tt-kirtanvaghela <135005218+sw-tt-kirtanvaghela@users.noreply.github.com>
2023-12-07 12:45:27 +0530

    Added updated asyncsocket files

---
+++ IoTConnect/IoTConnect.framework/Headers/IoTConnect.h
@@ -0,0 +1,23 @@
+//
+//  IoTConnect.h
+//  IoTConnect
+
+#import <Foundation/Foundation.h>
+
+//#import "GCDAsyncSocket.h"
+//#import "GCDAsyncUdpSocket.h"
+#import "MqttCocoaAsyncSocket.h"
+#import "MGCDAsyncSocket.h"
+#import "MGCDAsyncUdpSocket.h"
+
+
+//! Project version number for IoTConnect.
+FOUNDATION_EXPORT double IoTConnectVersionNumber;
+
+//! Project version string for IoTConnect.
+FOUNDATION_EXPORT const unsigned char IoTConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoTConnect/PublicHeader.h>
+
+
+
